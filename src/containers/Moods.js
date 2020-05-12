46259e28c9cefe8969fd9422bd964188d520b056
Fiona Ochs <fiona.oaks@gmail.com>
2020-05-12 15:40:25 -0700

    refactored handle selection

--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+
 import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
+import { addHyper } from '../../actions/hyperActions';
+import { addHungry } from '../actions/hungryActions';
+import { addTired } from '../actions/tiredActions';
+import { addEducated } from '../actions/educatedActions';
+import { getEducated } from '../selectors/educatedSelectors';
 
 export const isTired = state => state.coffees < 1 && state.naps < 1;
 export const isHyper = state => state.coffees > 3;
@@ -18,29 +26,41 @@ export const getFace = state => {
   return '😀';
 };
 
-export default class Moods extends Component {
-  state = {
-    coffees: 0,
-    snacks: 0,
-    naps: 0,
-    studies: 0
-  }
+const Moods = () => {
+  const dispatch = useDispatch();
+
+  // state = {
+  //   coffees: 0,
+  //   snacks: 0,
+  //   naps: 0,
+  //   studies: 0
+  // }
+
   //selectors
+  const coffees = useSelector(getHyper(state));
+  const snacks = useSelector(getHungry(state));
+  const naps = useSelector(state => getTired(state, naps));
+  const studies = useSelector(getEducated(state));
+
 
   //reducer
   handleSelection = action => {
     switch(action.type) {
       case 'DRINK_COFFEE':
-        this.setState(state => ({ coffees: state.coffees + 1 }));
+        dispatch(addHyper(coffees));
+        // this.setState(state => ({ coffees: state.coffees + 1 }));
         break;
       case 'EAT_SNACK':
-        this.setState(state => ({ snacks: state.snacks + 1 }));
+        dispatch(addHungry(snacks));
+        // this.setState(state => ({ snacks: state.snacks + 1 }));
         break;
       case 'TAKE_NAP':
-        this.setState(state => ({ naps: state.naps + 1 }));
+        dispatch(addTired(coffees, naps));
+        // this.setState(state => ({ naps: state.naps + 1 }));
         break;
       case 'STUDY':
-        this.setState(state => ({ studies: state.studies + 1 }));
+        dispatch(addEducated(studies));
+        // this.setState(state => ({ studies: state.studies + 1 }));
         break;
       default:
         console.log(`unhandled type: ${action.type}`);
@@ -48,16 +68,16 @@ export default class Moods extends Component {
   }
 
   render() {
-    const { coffees, snacks, naps, studies } = this.state;
-    const face = getFace(this.state);
+    // const { coffees, snacks, naps, studies } = this.state;
+    // const face = getFace(this.state);
 
     return (
       <>
         <Controls>
-          <button onClick={() => this.handleSelection({ type: 'DRINK_COFFEE' })}>coffee - {coffees}</button>
-          <button onClick={() => this.handleSelection({ type: 'EAT_SNACK' })}>snacks - {snacks}</button>
-          <button onClick={() => this.handleSelection({ type: 'TAKE_NAP' })}>naps - {naps}</button>
-          <button onClick={() => this.handleSelection({ type: 'STUDY' })}>studies - {studies}</button>
+          <button onClick={() => handleSelection({ type: 'DRINK_COFFEE' })}>coffee - {coffees}</button>
+          <button onClick={() => handleSelection({ type: 'EAT_SNACK' })}>snacks - {snacks}</button>
+          <button onClick={() => handleSelection({ type: 'TAKE_NAP' })}>naps - {naps}</button>
+          <button onClick={() => handleSelection({ type: 'STUDY' })}>studies - {studies}</button>
         </Controls>
         <Face emoji={face} />
       </>
